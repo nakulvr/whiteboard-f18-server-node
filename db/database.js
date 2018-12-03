a81d9273026a40d2918aa609298645cf9cef72be
vrnak <vrnakul@gmail.com>
2018-12-02 21:52:33 -0500

    added to Mlab + heroku

--- db/database.js
+++ db/database.js
@@ -1,8 +1,10 @@
 module.exports =  () => {
     const mongoose = require('mongoose');
-    const databaseName = 'whiteboard';
-    var   connectionString =
-        'mongodb://localhost/';
-    connectionString += databaseName;
+    // const databaseName = 'whiteboard';
+    const databaseName = 'heroku_x8dtzpdk'
+    // var   connectionString =
+    //     'mongodb://localhost/';
+    // connectionString += databaseName;
+    const connectionString = 'mongodb://test:test123@ds137100.mlab.com:37100/' + databaseName
     mongoose.connect(connectionString, { useNewUrlParser: true });
 };
